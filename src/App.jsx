724ebcd23c821840492bd59877c657fa25cbaf04
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-10-06 02:29:47 +0200

    adding jquery, react-loader-spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar/Navbar';
-import Quote from './Quotes/Quote';
 import Api from './Quotes/Api';
+import { InfinitySpin } from 'react-loader-spinner'
+import $ from 'jquery';
+import { useEffect } from 'react';
+
 
 function App() {
+  // window.addEventListener('load', () => {
+  // })
+  // $('spinner').hide()
+  useEffect(() => {
+    $(document).ready(function () {
+      $(".Page").show()
+
+    });
+
+    return () => {
+    }
+  }, [])
+
   return (
     <>
 
-    <div className='Page'>
-      <Navbar />
-        
-      <div className="App container d-flex justify-content-center ">
+      <div className='Page'>
+        <div className='landing'>
+        </div>
 
-        <Api />
-
+          <div className='spinner'>
+            <h1>Quoter App</h1>
+            <InfinitySpin
+              width='200'
+              color="#4fa94d"
+            />
+        </div>
+        <Navbar />
+        <div className="App container d-flex justify-content-center ">
+          <Api />
+        </div>
       </div>
-
-    </div>
-      
     </>
   );
 }
--- src/Quotes/Api.jsx
+++ src/Quotes/Api.jsx
@@ -1,23 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 import Quote from './Quote'
-
+import $, { easing } from 'jquery';
 
 
 export default function Api(props) {
   const [quote, setQuote] = useState('')
-
+  let respone = ''
   async function getFromApi() {
-    const respone = await Axios.get('https://api.quotable.io/random?minLength=20&maxLength=140')
+
+    respone = await Axios.get('https://api.quotable.io/random?minLength=20&maxLength=140')
     setQuote(respone.data.content)
+    $('.text').fadeIn(250);
+
   }
 
-  function generateNewQuote() {
-    getFromApi()
+  async function generateNewQuote() {
+    $('.text').fadeOut(500);
+    setTimeout(() => {
+      getFromApi();
+    }, 500)
+    rotateButton()
   }
+  function rotateButton() {
+    $('#diceButtonId svg').animate({
+      rotate: '0deg'
+    }, { duration: 500 }, "easein", $('#diceButtonId svg').animate({ rotate: '360deg' })
+    )
+    console.log('rotateButton')
+  }
+
+
 
   useEffect(() => {
     getFromApi()
+
 
     return () => {
 
@@ -25,7 +42,9 @@ export default function Api(props) {
   }, [])
 
   return (
+
     <>
+
       <Quote generateNewQuote={generateNewQuote} content={quote} />
     </>
   )
